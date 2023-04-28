4b1fbaa3551c881c172e6a3d22cd4c5dddccdb68
GianbaBotassi <gianba89@gmail.com>
2023-04-28 17:37:23 +0200

    Added reset button

--- js/script.js
+++ js/script.js
@@ -1,13 +1,10 @@
-    // Declare global var
-    const kmPrice = 0.21;   // Price/km 0,21€
-    let discount;
-    
+// Declare global var
+const kmPrice = 0.21;   // Price/km 0,21€
+let discount;
 
-
-
-    // Add event listener to button "See ticket"
-const sendForm = document.getElementById("seeTicket");
-sendForm.addEventListener("click",
+// Add event listener to button "See ticket"
+const seeTicket = document.getElementById("seeTicket");
+seeTicket.addEventListener("click",
     function(){
 
         // Added class on id "ticket" to show it
@@ -21,7 +18,7 @@ sendForm.addEventListener("click",
         const distance = document.getElementById("distance").value;
 
         // Initialize discount var with input
-        const offers = document.getElementById("offers").value;
+        const offers = document.getElementById("inputAge").value;
 
 
         if(offers == "<18 years old"){   // if under 18 years old 20% discount
@@ -31,13 +28,25 @@ sendForm.addEventListener("click",
         }else{  // else no discount
             discount = 0;
         }
-        console.log(discount);
 
+        //Declare and calculate ticket price with possible discount
         const amount = (distance * kmPrice);
         const sumToPay = amount - (amount * discount);
 
+        // Inner to html details and sum to pay
         document.getElementById("passengerName").innerHTML = userAge;
+        document.getElementById("offers").innerHTML = offers;
+        document.getElementById("price").innerHTML = sumToPay.toFixed(2);
     }
+    )
+
+
+    // Initialize button reset to remove ticket
+    const reset = document.getElementById("reset");
+    reset.addEventListener("click",
+        function(){
+            ticket.classList.remove("active"); 
+        }
     )
 
 
@@ -49,8 +58,3 @@ sendForm.addEventListener("click",
 
 
 
-//Declare and calculate ticket price with possible discount
-
-// Inner to html details and sum to pay
-
-// document.getElementById("ticket_price").innerHTML = sumToPay.toFixed(2);
